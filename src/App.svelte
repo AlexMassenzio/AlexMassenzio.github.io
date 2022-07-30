0aa8bfdf7113faf0f14d4451a380d7a18ef3aa83
unknown <alexmassenzio@gmail.com>
2022-07-30 16:49:57 -0400

    Added simple transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
 import { fly } from 'svelte/transition'
 import BGVid from "./lib/BGVid.svelte";
+import Contact from './lib/Contact.svelte';
+import Home from './lib/Home.svelte';
 import Navbar from "./lib/Navbar.svelte";
+import Projects from './lib/Projects.svelte';
 
 let navLoc:string;
+const ANIMATION_DURATION = 250;
 </script>
 
 <Navbar bind:navLoc={navLoc}/>
 
-<div class="columns p-contain" transition:fly="{{delay: 250, duration: 300, x: 100, y: 500, opacity: 0.5}}">
+{#if navLoc == "home"}
+  <div in:fly="{{x:400, duration: ANIMATION_DURATION, delay:ANIMATION_DURATION}}" out:fly="{{x:ANIMATION_DURATION, duration: ANIMATION_DURATION}}">
+    <Home/>
+  </div>
+{:else if navLoc == "projects"}
+  <div in:fly="{{x:400, duration: ANIMATION_DURATION, delay:ANIMATION_DURATION}}" out:fly="{{x:ANIMATION_DURATION, duration: ANIMATION_DURATION}}">
+    <Projects/>
+  </div>
+{:else if navLoc == "contact"}
+  <div in:fly="{{x:400, duration: ANIMATION_DURATION, delay:ANIMATION_DURATION}}" out:fly="{{x:ANIMATION_DURATION, duration: ANIMATION_DURATION}}">
+    <Contact/>
+  </div>
+{/if}
+
+<!-- <div class="columns p-contain" transition:fly="{{delay: 250, duration: 300, x: 100, y: 500, opacity: 0.5}}">
     <div class="column">
       First column
     </div>
@@ -20,11 +38,11 @@ let navLoc:string;
     </div>
 </div>
 
-<button class="button">{navLoc}</button>
+<button class="button">{navLoc}</button> -->
 
 <BGVid/>
 
-<style>
+<!-- <style>
     .p-contain {
         width: 300%;
     }
@@ -34,4 +52,4 @@ let navLoc:string;
         border-style:dashed;
         border-width: 10px;
     }
-</style>
+</style> -->
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,17 +1,41 @@
 <script lang="ts">
     export let navLoc : string = "home";
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Add a click event on each of them
+        navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const target_id = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                target_id.classList.toggle('is-active');
+
+            });
+        });
+    });
 </script>
 
 <nav class="navbar is-transparent">
-    <div class="navbar-burger" data-target="navItems">
-        <span></span>
-        <span></span>
-        <span></span>
+    <div class="navbar-brand">
+        <a class="navbar-item has-text-light" href="#home" on:click={() => {navLoc = "home"}}>Alex Massenzio</a>
+
+        <div class="navbar-burger" data-target="navItems">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 
     <div id="navItems" class="navbar-menu nbm">
         <div class="navbar-start">
-            <a class="navbar-item has-text-light" href="#home" on:click={() => {navLoc = "home"}}>Home</a>
             <a class="navbar-item has-text-light" href="#projects" on:click={() => {navLoc = "projects"}}>Projects</a>
             <a class="navbar-item has-text-light" href="#contact" on:click={() => {navLoc = "contact"}}>Contact</a>
         </div>
@@ -22,13 +46,11 @@
     .navbar {
         &.is-transparent{
             background-color: rgba($color: #070707, $alpha: 0.4);
-            //background: rgb(0,0,0);
-            //background: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.5) 40%, rgba(0,0,0,0.5) 100%);
         }
     }
 
     .navbar-start {
-        padding-left: 2rem;
+        //padding-left: 2rem;
     }
     
     .navbar-item {
