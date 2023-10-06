deb92c832d2636bd3a020ba5bd635b317fab1b70
Alex Massenzio <alexmassenzio@gmail.com>
2023-10-06 13:38:28 -0400

    footer no longer overlaps content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	class="bg-monochrome-100 dark:bg-monochrome-900 text-monochrome-900 dark:text-monochrome-100 font-body"
 >
 	<slot />
-	<footer class="fixed bottom-0 w-full text-center">
+	<footer class="w-full text-center">
 		<p>© Alex Massenzio 2016-{now.getFullYear()}</p>
 	</footer>
 </div>
