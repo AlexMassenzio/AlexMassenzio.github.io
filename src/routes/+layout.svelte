a22d77163674d1732a9c2c268b245cbc9d8a883f
Alex Massenzio <alexmassenzio@gmail.com>
2023-10-06 18:01:54 -0400

    formatting for footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,10 @@
 <div
 	class="relative min-h-[100dvh] box-border bg-monochrome-100 dark:bg-monochrome-900 text-monochrome-900 dark:text-monochrome-100 font-body"
 >
-	<main class="m-0 mx-auto pb-8">
+	<main class="m-0 mx-auto pb-12">
 		<slot />
 	</main>
-	<footer class="absolute bottom-0 w-full text-center">
+	<footer class="absolute bottom-0 mb-3 w-full text-center">
 		<div class="flex gap-x-2 justify-center">
 			<a href="/" class="text-info-500 dark:text-info-400 underline">Home</a>
 			<p>© Alex Massenzio 2016-{now.getFullYear()}</p>
