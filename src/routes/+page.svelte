455e05ab88189d3abe3f4a2420e1c18bfa510023
Alex Massenzio <alexmassenzio@gmail.com>
2023-12-24 10:04:43 -0500

    Added projects page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
 <div class="mt-4 h-full flex flex-col text-center">
 	<a class="text-info-500 dark:text-info-400 underline" href="/smxcalc">SMX Calc</a>
 	<a class="text-info-500 dark:text-info-400 underline" href="/pizzarank">Hoboken Pizza Rankings</a>
+	<a class="text-info-500 dark:text-info-400 underline" href="/projects">Projects</a>
 </div>
