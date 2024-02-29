0f27eb6dc32f0a01966ae6c5464116673facd416
Alex Massenzio <alexmassenzio@gmail.com>
2024-02-29 17:56:10 -0500

    Update pizzarank table style

--- src/routes/pizzarank/+page.svelte
+++ src/routes/pizzarank/+page.svelte
@@ -7,32 +7,42 @@
 <div class="pt-8 mx-2">
 	<h1 class="text-center text-xl sm:text-2xl">Alex's Hoboken Pizza Ranking</h1>
 	<table
-		class="mt-4 w-fit m-auto table-auto text-sm sm:text-lg text-left border-collapse border-2 border-monochrome-900 dark:border-monochrome-100"
+		class="mt-4 w-fit m-auto table-auto text-sm sm:text-lg text-left border-separate border-spacing-0 border rounded-lg border-monochrome-600 dark:border-monochrome-100"
 	>
 		<thead>
 			<tr>
-				<th class="p-2 border border-monochrome-900 dark:border-monochrome-100">Rank</th>
-				<th class="p-2 border border-monochrome-900 dark:border-monochrome-100">Name</th>
-				<th class="p-2 border border-monochrome-900 dark:border-monochrome-100">Go-To Item</th>
-				<th class="p-2 border border-monochrome-900 dark:border-monochrome-100">Info</th>
+				<th class="p-2 border rounded-tl-lg border-monochrome-600 dark:border-monochrome-500"
+					>Rank</th
+				>
+				<th class="p-2 border border-monochrome-600 dark:border-monochrome-500">Name</th>
+				<th class="p-2 border border-monochrome-600 dark:border-monochrome-500">Go-To Item</th>
+				<th class="p-2 border rounded-tr-lg border-monochrome-600 dark:border-monochrome-500"
+					>Info</th
+				>
 			</tr>
 		</thead>
 		<tbody>
 			{#each data.pizza as pizzaPlace, i}
 				<tr
-					class={(i + 1 == 1 ? 'dark:text-primary-300' : '') +
-						(pizzaPlace.goto == 'TBD' ? 'dark:text-monochrome-500 italic' : '')}
+					class={(i + 1 == 1 ? 'text-primary-400 dark:text-primary-300' : '') +
+						(pizzaPlace.goto == 'TBD'
+							? 'border-monochrome-600 dark:text-monochrome-500 italic'
+							: '')}
 				>
-					<td class="p-2 border border-monochrome-900 dark:border-monochrome-100"
-						>{pizzaPlace.goto == 'TBD' ? 'TBD' : i + 1}</td
+					<td
+						class="p-2 border {i == data.pizza.length - 1 ? 'rounded-bl-lg' : ''}
+							 border-monochrome-600 dark:border-monochrome-500">{pizzaPlace.goto == 'TBD' ? 'TBD' : i + 1}</td
 					>
-					<td class="p-2 border border-monochrome-900 dark:border-monochrome-100"
+					<td class="p-2 border border-monochrome-600 dark:border-monochrome-500"
 						>{pizzaPlace.name}</td
 					>
-					<td class="p-2 border border-monochrome-900 dark:border-monochrome-100"
+					<td class="p-2 border border-monochrome-600 dark:border-monochrome-500"
 						>{pizzaPlace.goto}</td
 					>
-					<td class="p-2 border border-monochrome-900 dark:border-monochrome-100 text-info-400">
+					<td
+						class="p-2 border {i == data.pizza.length - 1 ? 'rounded-br-lg' : ''}
+							 border-monochrome-600 dark:border-monochrome-500 text-info-400"
+					>
 						<a href={pizzaPlace.link} target="_blank">Link</a>
 					</td>
 				</tr>
