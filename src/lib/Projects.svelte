bcd8f87c68e7a30f3799648210e4e436944a5623
Alex Massenzio <alexmassenzio@gmail.com>
2024-04-07 13:51:35 -0400

    updated projects layout

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -4,38 +4,48 @@
 
 <article class="m-8">
 	{#each projectData as year}
-		<section class="m-2">
-			<h1 class="text-2xl underline">{year.year}</h1>
-			{#each year.projects as project}
-				<div class="my-2 p-2 border-2 rounded-sm flex flex-col sm:flex-row">
-					<div>
-						<h2 class="text-xl">{project.project}</h2>
-						<p class="mt-2">{project.description}</p>
-						<p class="mt-1">
-							{project.footer.text}<a
-								href={project.footer.link.url}
-								target="_blank"
-								rel="noopener noreferrer"
-								class="text-info-500 dark:text-info-400 underline"
-								>{project.footer.link.title}
-							</a>
-						</p>
-					</div>
-					{#if project.media}
-						<div class="mt-2 sm:mt-0 sm:ml-[25%]">
+		<section>
+			<h1 class="text-3xl text-center mb-4">{year.year}</h1>
+			<div class="flex flex-col items-center w-full gap-4 md:gap-6 mb-8">
+				{#each year.projects as project}
+					<div
+						class="border border-monochrome-500 md:w-1/2 bg-monochrome-800 h-fit rounded-xl flex flex-col p-2"
+					>
+						{#if project.media}
 							{#if !project.media.includes('gfycat')}
-								<figure>
+								<figure class="mb-4">
 									<img
 										src={project.media}
-										class="max-h-96"
+										class="w-full border border-monochrome-500 border-separate h-32 md:h-48 object-cover rounded-lg"
 										alt="preview of how '{project.project}' looks"
 									/>
 								</figure>
 							{/if}
+						{/if}
+						<div class="grid grid-cols-3 items-end mb-2">
+							<h2 class="col-span-2 text-2xl">{project.project}</h2>
+							<a
+								href={project.footer.link.url}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="text-info-500 dark:text-info-400 text-xl decoration-1 underline truncate text-end"
+								>{project.footer.link.title}
+							</a>
 						</div>
-					{/if}
-				</div>
-			{/each}
+						<p class="mb-2">{project.description}</p>
+						<p>
+							{project.footer.text}
+							<a
+								href={project.footer.link.url}
+								target="_blank"
+								rel="noopener noreferrer"
+								class="text-info-500 dark:text-info-400 decoration-1 underline truncate text-end"
+								>{project.footer.link.title}
+							</a>
+						</p>
+					</div>
+				{/each}
+			</div>
 		</section>
 	{/each}
 </article>
