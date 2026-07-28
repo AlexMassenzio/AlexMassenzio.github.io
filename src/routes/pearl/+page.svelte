dd98fac329cb49c8de55aeeb63c2636933d1b467
Alex Massenzio <alexmassenzio@gmail.com>
2026-07-28 18:32:09 -0400

    updated current year for pearl

--- src/routes/pearl/+page.svelte
+++ src/routes/pearl/+page.svelte
@@ -34,7 +34,7 @@
 	];
 </script>
 
-<h1 class="body-font text-center text-4xl font-bold">Pearl 2025 Cocktails</h1>
+<h1 class="body-font text-center text-4xl font-bold">Pearl 2026 Cocktails</h1>
 
 <div class="body-font mx-4">
 	{#each cocktailList as cocktail (cocktail.name)}
