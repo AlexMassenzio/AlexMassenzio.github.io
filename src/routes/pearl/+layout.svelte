d1efb37da0ee6a419bcd007bdde4914db0b71691
Alex Massenzio <alexmassenzio@gmail.com>
2026-07-28 17:34:16 -0400

    2026 pearl menu

---
+++ src/routes/pearl/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	/** @type {{children?: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Pearl St. Cocktails</title>
+	<link rel="icon" href="/cocktail.png" />
+	<meta name="description" content="Pearl St. Cocktails" />
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/pearl/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	type Cocktail = {
+		name: string;
+		notes: string;
+		ingredients: string;
+	};
+	const cocktailList: Cocktail[] = [
+		{
+			name: 'Mai Tai',
+			notes: '🍍 🏝️ 🍋‍🟩',
+			ingredients: 'Jamaica Rum, Acid Adjusted Pineapple Juice, Orgeat, Triple Sec, Lime.'
+		},
+		{
+			name: 'Caipirinha',
+			notes: '⛱ 🍋 🌊',
+			ingredients: 'Cachaça, Lime, Simple Syrup'
+		},
+		{
+			name: "Planter's Punch",
+			notes: '🌰 🍹',
+			ingredients:
+				'Lots of Jamaica Rum, Allspice Dram, Angostura Bitters, Demerara, Lime. While supplies last!'
+		},
+		{
+			name: "Viper's Tea",
+			notes: '🍵 ☁ 🌿',
+			ingredients: 'Yerba Mate Infused Pisco, Egg White, Lemon, Simple Syrup, Ango Bitters'
+		},
+		{
+			name: 'Paloma',
+			notes: '🌵 🍹 🍋',
+			ingredients: 'Tequila Blanco, Grapefruit Juice, Lime, Agave, Soda Water'
+		}
+	];
+</script>
+
+<h1 class="body-font text-center text-4xl font-bold">Pearl 2025 Cocktails</h1>
+
+<div class="body-font mx-4">
+	{#each cocktailList as cocktail (cocktail.name)}
+		<div class="mt-4 flex flex-row items-end gap-2">
+			<h2 class="text-2xl font-semibold underline">{cocktail.name}</h2>
+			<h3 class="text-lg">{cocktail.notes}</h3>
+		</div>
+		<h4 class="text-md mt-2 text-monochrome-900 dark:text-monochrome-200">
+			{cocktail.ingredients}
+		</h4>
+	{/each}
+</div>
+
+<p class="body-font mt-4 text-monochrome-800 dark:text-monochrome-300">
+	...or feel free to ask for anything else!
+</p>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&display=swap');
+	.body-font {
+		font-family: 'Edu AU VIC WA NT Hand', cursive;
+	}
+</style>
